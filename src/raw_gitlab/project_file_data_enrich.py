1f5ff9cec2c89279ea22e0a078c0932ce05e024d
dawnpatrol04 <dawnpatrol04@gmail.com>
2023-09-05 20:32:43 -0400

    fix

--- src/raw_gitlab/project_file_data_enrich.py
+++ src/raw_gitlab/project_file_data_enrich.py
@@ -1,14 +1,17 @@
 import pandas as pd
 from src.utils.database import connect_to_db
 import re
-
+import yaml
 def extract_docker_image(content):
     """Extract the image name from Dockerfile content."""
     match = re.search(r'^FROM\s+([^\s]+)', content, re.MULTILINE)
     return match.group(1) if match else None
 
  
-import yaml
+def extract_version_from_yaml(content):
+    """Extract the version from .gitlab-ci.yml content."""
+    data = yaml.safe_load(content)
+    return data.get('_version', None)
 
 def extract_gitlab_runner(content):
     """Extract the default runner tag from .gitlab-ci.yml content."""
@@ -36,9 +39,10 @@ def enrich_project_file_data():
         docker_df['image_name'] = docker_df['file_content'].apply(extract_docker_image)
         docker_df.to_sql('docker_search', conn, index=False, if_exists='replace')
 
-        # Process .gitlab-ci.yml files
+         # Process .gitlab-ci.yml files
         gitlabci_df = pd.read_sql("SELECT * FROM gitlabci_search", conn)
         gitlabci_df['runner_name'] = gitlabci_df['file_content'].apply(extract_gitlab_runner)
+        gitlabci_df['file_version'] = gitlabci_df['file_content'].apply(extract_version_from_yaml)  # Add this line to extract the version
         gitlabci_df.to_sql('gitlabci_search', conn, index=False, if_exists='replace')
 
         # Process .env files

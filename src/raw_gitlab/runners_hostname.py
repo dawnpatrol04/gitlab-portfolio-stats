13ca55e17af6f26d3c97bacd81a1d04c1f169262
dawnpatrol04 <dawnpatrol04@gmail.com>
2023-09-06 01:27:04 -0400

    test

--- src/raw_gitlab/runners_hostname.py
+++ src/raw_gitlab/runners_hostname.py
@@ -1,35 +1,16 @@
-import subprocess
+from scapy.all import srp, Ether, ARP
+from socket import gethostbyaddr
 
-def get_fqdn_from_ip(ip_address):
-    """Attempt to get FQDN from IP address using various methods."""
-    
-    commands = [
-        ["nslookup", ip_address],
-        ["dig", "-x", ip_address, "+short"],
-        ["host", ip_address],
-    ]
+def get_netbios_name(ip_address):
+    # First, we'll ARP for the MAC address of the target
+    resp, _ = srp(Ether(dst="ff:ff:ff:ff:ff:ff")/ARP(pdst=ip_address), timeout=2)
+    for _, rcv in resp:
+        mac_address = rcv[Ether].src
 
-    for command in commands:
-        try:
-            result = subprocess.check_output(command, stderr=subprocess.STDOUT, universal_newlines=True)
-            
-            # Parse results for FQDN
-            if command[0] == "nslookup":
-                lines = result.split('\n')
-                for line in lines:
-                    if "name = " in line:
-                        return line.split('name = ')[1].strip().rstrip('.')
-            elif command[0] in ["dig", "host"]:
-                return result.strip().rstrip('.')
-        except subprocess.CalledProcessError:
-            continue  # This command failed, move on to the next one
+    # Then we'll try to get the NetBIOS name
+    name = gethostbyaddr(mac_address)
+    return name[0] if name else None
 
-    return None  # All methods failed
-
-# Example usage:
-ip = "8.8.8.8"
-fqdn = get_fqdn_from_ip(ip)
-if fqdn:
-    print(f"The FQDN for IP {ip} is {fqdn}.")
-else:
-    print(f"Could not determine the FQDN for IP {ip}.")
+ip = "YOUR_IP_ADDRESS_HERE"
+hostname = get_netbios_name(ip)
+print(hostname)
